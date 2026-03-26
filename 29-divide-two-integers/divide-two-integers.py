719d84a55d047b19555b7c88e3a45112031d2412
YADLAPALLI PAVAN BHASKAR <pavanbhaskar933@gmail.com>
2026-03-26 08:43:53 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (99.95%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2**31 and divisor ==-1:
+            return (2**31)-1
+        else :
+            res=int(dividend/divisor)
+            return res
