1c72248d7b3d50ae704a63bb8cd9c1ba1ae77e83
YADLAPALLI PAVAN BHASKAR <pavanbhaskar933@gmail.com>
2026-06-22 11:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (91.40%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return count;
+            }
+            count++;
+        }
+        return -1;
+    }
+}
